b6c560f78db5b36d5377d2eb08e9201fccd0b136
aditidesai298 <chandanpro32@gmail.com>
2020-01-24 11:16:23 +0530

    [aditi] set the environment for mail

--- src/main/java/com/bridgelabz/fundoo/util/JwtGenerator.java
+++ src/main/java/com/bridgelabz/fundoo/util/JwtGenerator.java
@@ -6,12 +6,9 @@ import java.io.UnsupportedEncodingException;
 import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.interfaces.Claim;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.Verification;
+
 
 @SuppressWarnings("serial")
 @Component
