b9dbd7f7368255755afc0ec343d6f7f95368d397
aditidesai298 <chandanpro32@gmail.com>
2020-02-01 13:37:22 +0530

    [aditi] added delete note controller

--- src/main/java/com/bridgelabz/fundoo/controller/NoteController.java
+++ src/main/java/com/bridgelabz/fundoo/controller/NoteController.java
@@ -3,6 +3,8 @@ package com.bridgelabz.fundoo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -33,5 +35,21 @@ public class NoteController {
 			return ResponseEntity.status(HttpStatus.CREATED).body(new Response("Note created!", 201));
 		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Response("Error creating note", 400));
+	}
+	
+	@ApiOperation(value = "delete an existing note for valid user")
+	@ApiResponses(value = { 
+			@ApiResponse(code = 200, message = "note deleted!"),	
+			@ApiResponse(code = 300, message = "Note not found"),
+			@ApiResponse(code = 400, message = "Error deleting note"),
+			@ApiResponse(code = 401, message = "Authorization failed")})
+	@DeleteMapping("{id}/delete")
+	public ResponseEntity<Response> deleteNote(@PathVariable("id") long noteId, @RequestHeader("token") String token) {
+		if (nService.deleteNote(noteId, token)) {
+			return ResponseEntity.status(HttpStatus.OK).body(new Response("Note deleted! ", 200));
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body(new Response("Error deleting note ", 400));
+
 	}
 }
